ff0f1666bcc6212f438fc06ee2440c3678197d89
Pedro Roig <pedro.roiggimenez@soprasteria.com>
2019-04-03 13:33:02 +0200

    Error code parsing V2

--- app/src/main/java/com/pedroroig/example/androidunittesting/errorcodestest/ErrorCodesManager.kt
+++ app/src/main/java/com/pedroroig/example/androidunittesting/errorcodestest/ErrorCodesManager.kt
@@ -13,5 +13,28 @@ fun parseErrorCodeFromRawErrorBody(errorBody: String): Int {
     } catch (e: Exception) {
         0
     }
+}
+
+/**
+ * Parses server json error body to obtain the error code. The format can look like this:
+ * {"returnCode":104, "returnMessage":"Your number....
+ * or:
+ * {    "returnCode":400}
+ * If parsing does not succeed, return 0 to show generic error
+ */
+fun parseErrorCodeFromRawErrorBodyV2(errorBody: String): Int {
+    return try {
+        val withoutPrefix = errorBody
+            // remove all whitespaces
+            .replace("\\s".toRegex(), "")
+            // Remove prefix
+            .removePrefix("{returnCode:")
+        if(withoutPrefix.contains("returnMessage"))
+            withoutPrefix.substring(0, withoutPrefix.indexOf(',')).toInt()
+        else
+            withoutPrefix.substring(0, withoutPrefix.indexOf('}')).toInt()
+    } catch (e: Exception) {
+        0
+    }
 
 }
